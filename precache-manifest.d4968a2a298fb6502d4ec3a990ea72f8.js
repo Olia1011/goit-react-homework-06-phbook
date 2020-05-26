3fc649508910b57e327170ebfa390aa2a31332f5
Olia1011 <olgamelnyk10111989@gmail.com>
2020-05-26 19:35:42 +0300

    Updates

---
+++ precache-manifest.d4968a2a298fb6502d4ec3a990ea72f8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "686a6ed7e1f88f9a7dc701c79b82ab23",
+    "url": "/Olia1011/goit-react-homework-06-phbook/index.html"
+  },
+  {
+    "revision": "822be67659e7f81f8d6a",
+    "url": "/Olia1011/goit-react-homework-06-phbook/static/css/main.3e9427dd.chunk.css"
+  },
+  {
+    "revision": "e3b4e96a90734b4066cf",
+    "url": "/Olia1011/goit-react-homework-06-phbook/static/js/2.e259df83.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Olia1011/goit-react-homework-06-phbook/static/js/2.e259df83.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "822be67659e7f81f8d6a",
+    "url": "/Olia1011/goit-react-homework-06-phbook/static/js/main.fce6c2cf.chunk.js"
+  },
+  {
+    "revision": "64aa8f591b7ee66249b6",
+    "url": "/Olia1011/goit-react-homework-06-phbook/static/js/runtime-main.3ed036de.js"
+  },
+  {
+    "revision": "014c7f030ef0c10e1cbdd3b3e6f277ae",
+    "url": "/Olia1011/goit-react-homework-06-phbook/static/media/dancing-script-v14-latin-regular.014c7f03.woff"
+  },
+  {
+    "revision": "5c1198f377ebf0fe64dd1a42476f0309",
+    "url": "/Olia1011/goit-react-homework-06-phbook/static/media/dancing-script-v14-latin-regular.5c1198f3.woff2"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/Olia1011/goit-react-homework-06-phbook/precache-manifest.d4968a2a298fb6502d4ec3a990ea72f8.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Olia1011/goit-react-homework-06-phbook/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
